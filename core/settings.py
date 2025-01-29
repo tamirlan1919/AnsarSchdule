2c0e0092ac48486eea873e0427285c2f1e4d75c2
tamirlan1919 <tchinchaev@bk.ru>
2025-01-29 14:01:50 +0300

    Изменил бд на PostgreSQL

--- core/settings.py
+++ core/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'default_db',
+        'USER': 'gen_user',
+        'PASSWORD': "T@)2NBj?}W:CjC",
+        'HOST': '89.23.117.56',
+        'PORT': '5432',
     }
 }
 
